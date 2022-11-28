de6fc8a7fa2205e68fa57a4a32858a4d93c2559f
andrewzhivolup <ferari1190@mail.ru>
2022-11-28 15:29:05 +0300

    добавил проверку на старте

--- 6 kyu/Banker's Plan/fortune.js
+++ 6 kyu/Banker's Plan/fortune.js
@@ -7,12 +7,19 @@
  * @param {number} i - Процент инфляции
  */
 const fortune = (f0, p, c0, n, i) => {
+    if (f0 < c0) { return false }
     let f1 = f0
     let c1 = c0
     for (let index = 1; index <= n - 1; index++) {
-        f1 = Math.round(f1 + f1 * p * 0.01 - c1)
-        c1 = Math.round(c1 + c1 * i * 0.01)
+        f1 = f1 + f1 * p * 0.01 - c1
+        c1 = c1 + c1 * i * 0.01
     }
-    return f2 >= 0
+    return f1 >= 0
 }
+console.log(fortune(9999, 61.8161, 10000, 3, 0));
+
+// 100000000 1 100000 50 1
+// 100000000 1.5 10000000 50 1
+// 100000000 5 1000000 50 1
+// 9999 61.8161 10000 3 0
 module.exports = fortune;
