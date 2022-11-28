08eb6ca997fb9e363ffe4faaa4a144dc9ecbef3a
AZhivolup <ferari1190@mail.ru>
2022-11-28 18:35:50 +0300

    решение задачи Calculating with Functions

---
+++ 5 kyu/Calculating with Functions/func.js
@@ -0,0 +1,72 @@
+const zero = (param) => {
+  return expression(0, param);
+};
+const one = (param) => {
+  return expression(1, param);
+};
+const two = (param) => {
+  return expression(2, param);
+};
+const three = (param) => {
+  return expression(3, param);
+};
+const four = (param) => {
+  return expression(4, param);
+};
+const five = (param) => {
+  return expression(5, param);
+};
+const six = (param) => {
+  return expression(6, param);
+};
+const seven = (param) => {
+  return expression(7, param);
+};
+const eight = (param) => {
+  return expression(8, param);
+};
+const nine = (param) => {
+  return expression(9, param);
+};
+
+const plus = (num) => {
+  return function (y) {
+    return y + num;
+  };
+};
+const minus = (num) => {
+  return function (y) {
+    return y - num;
+  };
+};
+const times = (num) => {
+  return function (y) {
+    return y * num;
+  };
+};
+const dividedBy = (num) => {
+  return function (y) {
+    return Math.floor(y / num);
+  };
+};
+
+const expression = (num, param) => {
+  if (!param) return num;
+  return param(num);
+};
+
+module.exports = {
+  plus,
+  minus,
+  times,
+  dividedBy,
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+};
---
+++ 5 kyu/Calculating with Functions/func.test.js
@@ -0,0 +1,30 @@
+const {
+  one,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+  plus,
+  minus,
+  times,
+  dividedBy,
+} = require("./func");
+
+describe("Calculating with Functions", () => {
+  test("1+2+9=12", () => {
+    expect(one(plus(two(plus(nine()))))).toBe(12);
+  });
+  test("4-3-0=1", () => {
+    expect(four(minus(three(minus(zero()))))).toBe(1);
+  });
+  test("5*6=30", () => {
+    expect(five(times(six()))).toBe(30);
+  });
+  test("8/7=1", () => {
+    expect(eight(dividedBy(seven()))).toBe(30);
+  });
+});
