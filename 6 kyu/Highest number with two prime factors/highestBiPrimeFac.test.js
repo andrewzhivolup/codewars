c91f2fad09694bdb55c5837ca7d2620e2565faff
andrewzhivolup <ferari1190@mail.ru>
2023-08-24 21:01:52 +0300

    repair tests

--- 6 kyu/Highest number with two prime factors/highestBiPrimeFac.test.js
+++ 6 kyu/Highest number with two prime factors/highestBiPrimeFac.test.js
@@ -2,10 +2,14 @@ const highestBiPrimeFac = require("./highestBiPrimeFac");
 
 describe("Highest number with two prime factors", () => {
     test("test 1", () => {
-        expect(highestBiPrimeFac((2, 3, 50))).toEqueal([48, 4, 1]);
+        const actual = highestBiPrimeFac(2, 3, 50);
+        const answer = [48, 4, 1];
+        expect(actual).toEqual(answer);
     });
     test("test 2", () => {
-        expect(highestBiPrimeFac((5, 11, 1000))).toEqueal([605, 1, 2]);
+        const actual = highestBiPrimeFac(5, 11, 1000);
+        const answer = [605, 1, 2];
+        expect(actual).toEqual(answer);
     });
 });
 
